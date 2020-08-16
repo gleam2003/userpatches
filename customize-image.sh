61ce0ecccc8c90c4d43c4b25150c38e2dc48f636
Galileo <43069043+abiurare@users.noreply.github.com>
2020-08-16 11:11:21 +0200

    Update

--- customize-image.sh
+++ customize-image.sh
@@ -28,28 +28,53 @@ Main() {
 	adduser pi --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 	echo pi:raspberry | chpasswd
 
-	mkdir -p /etc/systemd/system/getty@tty1.service.d/
-    cat >/etc/systemd/system/getty@tty1.service.d/autologin.conf <<_EOF_
-[Service]
-ExecStart=
-ExecStart=-/sbin/agetty --autologin pi --noclear %I \$TERM
-_EOF_
+	#mkdir -p /etc/systemd/system/getty@tty1.service.d/
+    #cat >/etc/systemd/system/getty@tty1.service.d/autologin.conf <<_EOF_
+#[Service]
+#ExecStart=
+#ExecStart=-/sbin/agetty --autologin pi --noclear %I \$TERM
+#_EOF_
 
 	git clone https://github.com/gleam2003/RetroPie-Setup /home/pi/RetroPie-Setup
 	chown pi /home/pi/RetroPie-Setup -R
 
-	tar -xhzvf /tmp/overlay/include.tar.gz -C /
-	tar -xhzvf /tmp/overlay/maliuserpsace.tar.gz -C /
+	tar -xhzvf /tmp/overlay/usr.tar.gz -C /
+	tar -xhzvf /tmp/overlay/etc.tar.gz -C /
+	#tar -xhzvf /tmp/overlay/opt.tar.gz -C /
+	#tar -xhzvf /tmp/overlay/home.tar.gz -C /
 
 	cp -r /tmp/overlay/etc/ /
 
-	dpkg -i /tmp/overlay/rk3399/sdl2/libsdl2-2.0-0_2.0.10+5_arm64.deb  /tmp/overla/rk3399/sdl2/libsdl2-dev_2.0.10+5_arm64.deb
+	#dpkg -i /tmp/overlay/rk3399/sdl2/libsdl2-2.0-0_2.0.10+5_arm64.deb  /tmp/overlay/rk3399/sdl2/libsdl2-dev_2.0.10+5_arm64.deb
+	
 
-	#su -c "sudo -S __platform=rk3399 __nodialog=1 /home/pi/RetroPie-Setup/retropie_packages.sh retroarch" - pi
-	#su -c "sudo -S __platform=rk3399 __nodialog=1 /home/pi/RetroPie-Setup/retropie_packages.sh emulationstation-dev" - pi
+cat >/home/pi/install.sh <<_EOF_
+#!/bin/bash
+cd
+sudo apt-get update
+sudo apt-get -y install git dialog xmlstarlet joystick
+git clone https://github.com/gleam2003/RetroPie-Setup.git
+cd RetroPie-Setup
+modules=(
+    'setup basic_install'
+    'autostart enable'
+)
+for module in "\${modules[@]}"; do
+    sudo __platform=rk3399 __nodialog=1 ./retropie_packages.sh \$module
+done
+rm -rf tmp
+sudo apt-get clean
+_EOF_
 
-	echo abiuplayer | sudo -S su -l pi -c '__platform=rk3399 __nodialog=1 /home/pi/RetroPie-Setup/retropie_packages.sh retroarch'
-	echo abiuplayer | sudo -S su -l pi -c '__platform=rk3399 __nodialog=1 /home/pi/RetroPie-Setup/retropie_packages.sh emulationstation'
+#su -c "sudo -S /home/pi/install.sh" - pi	
+
+	su -c "sudo -S __platform=rk3399 __nodialog=1 /home/pi/RetroPie-Setup/retropie_packages.sh setup basic_install" - pi
+	su -c "sudo -S __platform=rk3399 __nodialog=1 /home/pi/RetroPie-Setup/retropie_packages.sh autostart enable" - pi
+	#su -c "sudo -S __platform=rk3399 __nodialog=1 /home/pi/RetroPie-Setup/retropie_packages.sh retropiemenu" - pi
+	#su -c "sudo -S __platform=rk3399 __nodialog=1 /home/pi/RetroPie-Setup/retropie_packages.sh runcommand" - pi
+
+	#echo abiuplayer | sudo -S su -l pi -c '__platform=rk3399 __nodialog=1 /home/pi/RetroPie-Setup/retropie_packages.sh retroarch'
+	#echo abiuplayer | sudo -S su -l pi -c '__platform=rk3399 __nodialog=1 /home/pi/RetroPie-Setup/retropie_packages.sh emulationstation'
 
 } # Main
 
